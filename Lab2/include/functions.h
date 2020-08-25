2c3b4b1ed7d5892b5325def8fc4a65ac06609653
Jonathan Abbott <jtabbott2001@gmail.com>
2020-08-25 16:23:20 -0400

    Finished comments and tests.

--- Lab2/include/functions.h
+++ Lab2/include/functions.h
@@ -16,13 +16,27 @@
  *  Prints a given vector into standard output.
  *  
  *  @param vec The vector to be printed.
+ *  @param delim The delimiter to be printed after values.
  */
 template<class T>
-void PrintVector(const std::vector<T> & vec) {
-    for (size_t i = 0; i < vec.size(); ++i) {
-        std::cout << vec.at(i) << " ";
+void PrintVector(const std::vector<T> & vec, bool space=true) {
+
+    
+    // if vector is empty, print out that the list is empty.
+    if (vec.empty()) {
+        std::cout << "List is Empty" << std::endl;
+    
+
+    // if not, print out delimited values.
+    } else {
+        for (size_t i = 0; i < vec.size(); ++i) {
+            std::cout << vec.at(i);
+            if (space) {
+                std::cout << " ";
+            }
+        }
+        std::cout << std::endl;
     }
-    std::cout << std::endl;
 }
 
 
@@ -35,18 +49,37 @@ void PrintVector(const std::vector<T> & vec) {
  *  @return Value in interval [0, piles.size()]. piles.size() means that a new pile must be made.
  */
 template<class T>
-int LeftmostSolitairePlacement(const std::vector<T> & piles, T card) {
-    int l = 0, r = piles.size();
+size_t LeftmostSolitairePlacement(const std::vector<T> & piles, T card) {
+
+
+    // binary search with [0, piles.size()] as bounds.
+    // we do `piles.size()` because we want the ability for this function to return
+    // a case where a new pile must be made.
+    size_t l = 0, r = piles.size();
+
+
+    // iterate until `r` "closes in on" `l`.
     while (l < r) {
-        int m = (l + r) / 2;
+
+        
+        // find the averge of `l` and `r` to find the middle.
+        size_t m = (l + r) / 2;
+
+
+        // if the card at the middle is not valid for placement, move `l` one past middle.
         if (piles[m] < card) {
             l = m + 1;
-            // not "l = m" because we don't care about piles[m] if it's < card.
+        
+
+        // else, move `r` (right bound) to middle.
         } else {
             r = m;
-            // not "r = m - 1" because we care if piles[m] >= card.
         }
     }
+
+
+    // `l` because either the minumum available index or piles.size() which means
+    // that a new pile must be made.
     return l;
 }
 
@@ -60,9 +93,13 @@ int LeftmostSolitairePlacement(const std::vector<T> & piles, T card) {
  *  @param elem The element to be inserted.
  */
 template<class T>
-void OverwriteOrAppend(std::vector<T> & vec, int index, T elem) {
+void OverwriteOrAppend(std::vector<T> & vec, size_t index, T elem) {
+    
+    // if index is one index out of bounds, create a new pile.
     if (index == vec.size()) {
         vec.push_back(elem);
+    
+    // else, replace the element at index with `elem`.
     } else {
         vec.at(index) = elem;
     }
@@ -71,7 +108,7 @@ void OverwriteOrAppend(std::vector<T> & vec, int index, T elem) {
 
 /**
  *  Finds the length of the longest increasing subsequence, inspired by patience
- *  sorting. Take out a deck of cards and sort them one-by-one following these rules:
+ *  sorting. Take out a deck of cards and deal them one-by-one following these rules:
  *      - A card must be placed on a card of equal or greater value.
  *      - A card can be placed in a new pile (to the right).
  *  Notice that the bottom-most cards are always in increasing order from left to right.
@@ -79,17 +116,31 @@ void OverwriteOrAppend(std::vector<T> & vec, int index, T elem) {
  *  increasing subsequence. The following algorithm implements this precisely.
  *  
  *  Note: Since a binary search O(logn) is done n times, this algorithm runs in O(nlogn) time.
+ *  And since the maximum length for an increasing subsequence is limited by the length of the input
+ *  vector, this algorithm has O(n) space complexity.
  *  
  *  @param deck The vector in which the length of the longest increasing subsequence will be found.
  *  @return An integer denoting the length of the longest increasing subsequence in the given vector.
  */
 template<class T>
-int LongestIncreasingSubsequence(const std::vector<T> & deck) {
+size_t LongestIncreasingSubsequenceLength(const std::vector<T> & deck) {
+
+    
+    // store the smallest in each pile.
     std::vector<T> piles;
     for (T card : deck) {
-        int index = LeftmostSolitairePlacement(piles, card);
+        
+        // find leftmost valid pile.
+        size_t index = LeftmostSolitairePlacement(piles, card);
+        
+        // place card on pile or create pile if it does not exist.
         OverwriteOrAppend(piles, index, card);
     }
+
+
+    // piles.size() is the length of the maximum increasing subsequence.
+    // because, if you look at one pile, cards that are stacked on top are
+    // newer with smaller value. ergo, an increasing subsequence can only
+    // use one card from each pile.
     return piles.size();
 }
-
--- Lab2/src/Lab2.cpp
+++ Lab2/src/Lab2.cpp
@@ -1,22 +1,91 @@
+/**
+ *  @author Jonathan Abbott
+ *  @date   Aug 25, 2020
+ * 
+ *  This file includes the `main` driver for Lab2, including the tests
+ *  for finding the longest increasing subsequence in an array.
+ */
+
+
 #include <iostream>
 #include <vector>
+#include <string>
 
 #include "../include/functions.h"
 
-int main(int arc, char ** argv) {
-    std::vector<int> MyVec;
-    MyVec.push_back(0);
-    MyVec.push_back(1);
-    MyVec.push_back(2);
-    MyVec.push_back(3);
-    MyVec.push_back(4);
-    MyVec.push_back(5);
-    MyVec.push_back(7);
-    MyVec.push_back(9);
-    MyVec.push_back(10);
-    MyVec.push_back(18);
-    MyVec.push_back(101);
-    PrintVector(MyVec);
-    std::cout << LongestIncreasingSubsequence(MyVec) << std::endl;
+
+/**
+ *  This is the `main` driver of the program.
+ * 
+ *  @return An integer exit code (i.e. 0 for success).
+ */
+int main() {
+
+
+    // first test, test edge case.
+    std::vector<int> EmptyVector;
+    std::cout << "----------------------" << std::endl;
+    std::cout << "Empty Array Test:" << std::endl;
+    std::cout << "Array to Test for LIS\t: ";
+    PrintVector(EmptyVector);
+    std::cout << "Result: " << LongestIncreasingSubsequenceLength(EmptyVector) << std::endl;
+    std::cout << "----------------------" << std::endl;
+
+
+    // test normal signed integers. GIVEN #1
+    std::vector<int> SignedTest{
+        10, 9, 2, 5, 3, 7, 101, 18
+    };
+    std::cout << "Signed Integer Test:" << std::endl;
+    std::cout << "Array to Test for LIS\t: ";
+    PrintVector(SignedTest);
+    std::cout << "Result: " << LongestIncreasingSubsequenceLength(SignedTest) << std::endl;
+    std::cout << "----------------------" << std::endl;
+
+
+    // test unsigned integers. GIVEN #2
+    std::vector<unsigned int> UnsignedTest{
+        10, 5, 8, 3, 9, 4, 12, 11
+    };
+    std::cout << "Unsigned Integer Test:" << std::endl;
+    std::cout << "Array to Test for LIS\t: ";
+    PrintVector(UnsignedTest);
+    std::cout << "Result: " << LongestIncreasingSubsequenceLength(UnsignedTest) << std::endl;
+    std::cout << "----------------------" << std::endl;
+
+
+    // test characters. GIVEN #3
+    std::vector<char> CharTest{
+        'e', 'A', 'B', 'a', 'C', 'f', 'D', 'E'
+    };
+    std::cout << "ASCII Character Test" << std::endl;
+    std::cout << "Array to Test for LIS\t: ";
+    PrintVector(CharTest);
+    std::cout << "Result: " << LongestIncreasingSubsequenceLength(CharTest) << std::endl;
+    std::cout << "----------------------" << std::endl;
+
+
+    // test string by passing string's properties to vector. CREATED #1 STRING
+    std::string MyString = "The last thing you'd want in your Burger King burger is someone's foot fungus. But as it turns out, that might be what you get.";
+    std::vector<char> StringTest(MyString.begin(), MyString.end());
+    std::cout << "String Test:" << std::endl;
+    std::cout << "Array to Test for LIS\t: ";
+    PrintVector(StringTest, false);
+    std::cout << "Result: " << LongestIncreasingSubsequenceLength(StringTest) << std::endl;
+    std::cout << "----------------------" << std::endl;
+
+
+    // test another set of unsigned integers. CREATED #2 UNSIGNED INT
+    std::vector<unsigned int> SecondUnsignedTest{
+        38, 26, 32, 41, 12, 7, 22, 19, 35, 37, 48, 7, 29, 11, 47, 30, 35, 22, 27, 24, 47, 34, 36, 0, 14
+    };
+    std::cout << "Second Unsigned Test:" << std::endl;
+    std::cout << "Array to Test for LIS\t: ";
+    PrintVector(SecondUnsignedTest);
+    std::cout << "Result: " << LongestIncreasingSubsequenceLength(SecondUnsignedTest) << std::endl;
+    std::cout << "----------------------" << std::endl;
+
+
+    // EXIT_SUCCESS.
     return 0;
 }
