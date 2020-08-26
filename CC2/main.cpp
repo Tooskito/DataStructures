d3fe11f3f04b981eea287fead0c6c4efd7a1814c
Jonathan Abbott <jtabbott2001@gmail.com>
2020-08-26 13:45:56 -0400

    More work. Nice debugging framework.

--- CC2/main.cpp
+++ CC2/main.cpp
@@ -6,33 +6,49 @@ using namespace std;
 
 template<class T>
 void printv(const vector<T> & vec) {
+    
+    // print vector.
     for (T elem : vec) {
         cout << elem << " ";
     }
     cout << endl;
 }
 
+
 template<class T>
 void printvp(const vector<T> & vec, size_t p, size_t l, size_t r) {
+
+    // print vector.
     printv(vec);
 
+    // print pointer vector.
     vector<char> pv(vec.size(), ' ');
     pv.at(p) = 'p';
     pv.at(l) = 'l';
     pv.at(r) = 'r';
     printv(pv);
 
-    /*
-    vector<char> lv(vec.size(), ' ');
-    lv.at(l) = 'l';
-    printv(lv);
+    // print divider.
+    for (size_t i = 0; i < vec.size() * 2 - 1; ++i) {
+        cout << "-";
+    }
+    cout << endl;
+}
 
-    vector<char> rv(vec.size(), ' ');
-    rv.at(r) = 'r';
-    printv(rv);
-    */
 
-    for (size_t i = 0; i < vec.size() * 2; ++i) {
+template<class T>
+void swapvp(vector<T> & vec, size_t a, size_t b) {
+
+    // swaps indices a and b.
+    T temp = vec.at(a);
+    vec.at(a) = vec.at(b);
+    vec.at(b) = temp;
+
+    // print out after swap.
+    printv(vec);
+
+    // print divider.
+    for (size_t i = 0; i < vec.size() * 2 - 1; ++i) {
         cout << "-";
     }
     cout << endl;
@@ -40,32 +56,12 @@ void printvp(const vector<T> & vec, size_t p, size_t l, size_t r) {
 
 template <class T>
 void quicksort(vector<T> & vec) {
+    
+    // initial print.
     size_t p = 0, l = 1, r = vec.size() - 1;
     printvp(vec, p, l, r);
-    while (l < r) {
-        while (vec.at(l) < vec.at(p) and l < r) {
-            ++l;
-        }
-        while (vec.at(r) > vec.at(p) and r > l) {
-            --r;
-        }
-        T temp = vec.at(l);
-        vec.at(l) = vec.at(r);
-        vec.at(r) = temp;
-    }
-    T temp = vec.at(p);
-    vec.at(p) = vec.at(--r);
-    vec.at(r) = temp;
+
     
-    printv(vec);
-    while (vec.at(l) < vec.at(p) and l < r) {
-        ++l;
-        cout << "l is now " << l << " where the val is " << vec.at(l) << endl;
-    }
-    while (vec.at(r) > vec.at(p) and r > l) {
-        --r;
-        cout << "r is now " << r << " where the val is " << vec.at(r) << endl;
-    }
 }
 
 int main() {
